e7a245f78ba3bc6d54dd9cc7b79005a8e5f160dd
Ikramullah <ikramullahzx@gmail.com>
2024-03-01 21:58:14 +0800

    feat(app): ready for clicking, prepare for arduino

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import getViewport from "GetViewport.js";
 // Summon browser and page
 const browser = await puppeteer.launch({
     headless: false,
+    userDataDir: "./userData",
     args: [
         "--start-fullscreen",
         "--window-position=0,0"
@@ -11,5 +12,15 @@ const browser = await puppeteer.launch({
 });
 const [page] = await browser.pages();
 
+// Setup browser
 await page.setViewport(getViewport());
 await page.goto("https://wheelofnames.com/");
+
+// Get element of handler
+const spinButton = await page.waitForSelector("#wheelCanvas");
+if (spinButton === null) {
+    throw new Error("No spin button located");
+}
+
+// Click the button
+await spinButton.click();
